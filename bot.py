98596efe97b028c223448c97de3e2228163e7be3
ketoru <ntsechov@gmail.com>
2024-02-11 05:41:48 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,194 @@
+from ultralytics import YOLO
+import telebot
+from telebot import types
+from telebot.types import InputFile, InputMediaVideo
+import os
+import pandas as pd
+from PIL import Image
+import io
+import numpy as np
+import yaml
+from helpers import *
+
+clear_temp_dirs()
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+bot = telebot.TeleBot(config['token'])
+print("i'm awake")
+strt = types.BotCommand('start', 'Вернуться в начало')
+detect = types.BotCommand('detect', 'Определить породу')
+metrcis = types.BotCommand('metrics', 'Метрики')
+about = types.BotCommand('about', 'О проекте')
+bot.set_my_commands(commands= [strt,detect,metrcis,about])
+
+goodbyes = ['поки','пока', 'до связи']
+alo = ['ало', 'оло','олу','алу','алло','олло']
+thanks = ['спасибо', 'сенкс', 'спс', 'спасибки']
+greeting = '\n'.join(["Привет, я котенок по имени Гав.",
+        "Я знаю более 100 различных пород собак и помогу тебе определить породу по картинке или видео",
+        "Выбирай 'Определить породу' или просто отправь мне фотографию или видео"
+        ])
+cl_map = read_class_map('class_mapping.csv')
+
+
+path_to_weights = os.path.join(os.getcwd(),'weights/best_60.pt')
+model = YOLO(path_to_weights)
+print('model loaded')
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    get_msg_data(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn0 = types.KeyboardButton("Определить породу")
+    btn1 = types.KeyboardButton("Метрики")
+    btn2 = types.KeyboardButton("О проекте")
+    markup.add(btn0)
+    markup.add(btn1)
+    markup.add(btn2)
+    bot.send_document(message.from_user.id, document="CAACAgIAAxkBAAElhJVk79KGyUEEP_9qejuYOc3JWIsLwwACjhcAAhspoEhGVp2TgPvWUTAE")
+    bot.send_message(message.from_user.id, greeting, reply_markup=markup)
+
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    get_msg_data(message)
+    msg = bot.send_message(message.from_user.id, 'Секундочку...')
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    img = Image.open(io.BytesIO(downloaded_file))
+    preds = model.predict(img,conf = 0.15)
+    bot.edit_message_text('Обработка завершена',message.from_user.id,msg.message_id)
+    if len(preds[0].boxes.cls) != 0:
+        img = np.array(img)
+        img_to_save = illustrate_boxes(preds[0],cl_map,0.8,img)
+        # img_to_save = Image.fromarray(img_to_save)
+        save_path = 'saved_temp/photo_from_{}.jpg'.format(message.from_user.username)
+        img_to_save.save(save_path)
+        bot.send_photo(message.from_user.id,InputFile(save_path))
+        bot.delete_message(message.from_user.id,msg.message_id)
+        if os.path.isfile(save_path):
+            os.remove(save_path)
+    else:
+        bot.edit_message_text('К сожалению ничего не нашлось, попробуйте другое фото',message.from_user.id,msg.message_id)
+
+
+@bot.message_handler(content_types=['video'])
+def handle_video(message):
+    get_msg_data(message)
+    stick = bot.send_document(message.from_user.id, document="CAACAgQAAxkBAAEpWiRlxFdyBAdzL04Db6M3XBuRdyYwJQAC_wADD6r0B6U5nuntNbspNAQ")
+    msg = bot.send_message(message.from_user.id, 'Секундочку...')
+    file_info = bot.get_file(message.video.file_id)
+    bot.edit_message_text('Загружаю видео',message.from_user.id,msg.message_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    print('downloaded video')
+    output_file = 'saved_temp/video_from_{}.avi'.format(message.from_user.username)
+    with open(output_file, "wb") as out_file: 
+        out_file.write(downloaded_file)
+    print('file written')
+    bot.edit_message_text('Обрабатываю',message.from_user.id,msg.message_id)
+    model.predict(output_file,project = 'video_preds',name = 'temp',save = True,exist_ok=True )
+    os.remove(output_file)
+    bot.edit_message_text('Конвертирую в mp4',message.from_user.id,msg.message_id)
+    pred_path = 'video_preds/temp/video_from_{}'.format(message.from_user.username)
+    convert_avi_to_mp4(pred_path+'.avi',pred_path+'.mp4')
+    print('converted')
+    bot.edit_message_text('Осталось совсем чуть-чуть',message.from_user.id,msg.message_id)
+    bot.send_video(message.from_user.id,InputFile(pred_path+'.mp4'))
+    bot.delete_message(message.from_user.id,stick.message_id)
+    bot.delete_message(message.from_user.id,msg.message_id)
+    print('sent')
+    os.remove(pred_path+'.mp4')
+
+
+@bot.message_handler(commands=['detect'])
+def start_detection_dialog(message):
+    get_msg_data(message)
+    dtc_msg = bot.send_message(message.from_user.id, 'Отправь мне фото или видео и я определю породы собак на нем')
+    bot.register_next_step_handler(dtc_msg,find_breeds)
+def find_breeds(message):
+    get_msg_data(message)
+    if message.photo:
+        handle_photo(message)
+    elif message.video:
+        handle_video(message)
+    else:
+        dtc_msg = bot.send_message(message.from_user.id, 'Это не фото и не видео')
+        bot.register_next_step_handler(dtc_msg,find_breeds)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Определить породу')
+def start_detection_dialog_(message):
+    start_detection_dialog(message)
+
+
+@bot.message_handler(commands=['metrcis'])
+def send_metrics(message):
+    get_msg_data(message)
+    bot.send_message(message.from_user.id,'тут будут метрики')
+
+@bot.message_handler(commands=['about'])
+def send_about(message):
+    get_msg_data(message)
+    bot.send_message(message.from_user.id,'тут будет about')
+
+@bot.message_handler(func=lambda message: message.text == 'Метрики')
+def send_metrics_(message):
+    send_metrics(message)
+
+@bot.message_handler(func=lambda message: message.text == 'О проекте')
+def send_about_(message):
+    send_about(message)
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    get_msg_data(message)
+    if str(message.text).lower() in goodbyes:
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='github', url='https://guthib.com/')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, 'Пока, если тебе понравилось, посмотри мои остальные проекты на github',reply_markup=markup)
+        bot.send_document(message.from_user.id, document="CAACAgIAAxkBAAElhLVk7-Laa9CXFIWa6Tj1l8vB9-_WfQACGBkAAp4WoUhNBRp4s3ZdGzAE")
+
+    elif str(message.text).lower() ==  'хуй':
+        bot.send_document(message.from_user.id, document="CAACAgQAAxkBAAElhLdk7-PhZBJS1t5ejfNSAAHZq0HjkKwAAvsAAw-q9AdB7mhhzhiekTAE")
+
+    elif str(message.text).lower() in thanks:
+        bot.send_message(message.from_user.id, 'Всегда пожалуйста')
+        bot.send_sticker(message.from_user.id, 'CAACAgIAAxkBAAElifBk8KwDUqQTS0au9nwo6hqSkyNCQgACoBwAAipooUjogwEq_q_PRzAE')
+
+    elif str(message.text).lower() in alo:
+        bot.send_message(message.from_user.id, 'Ало, это я, Пригожин Женя')
+        bot.send_sticker(message.from_user.id, "CAACAgIAAxkBAAEligJk8K1OwDcyyDW-WW90RDyUZ7F_9wAC7RgAAuJ1oUhGn01X81OLfjAE")
+
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn0 = types.KeyboardButton("Определить породу")
+        btn1 = types.KeyboardButton("Метрики")
+        btn2 = types.KeyboardButton("О проекте")
+        markup.add(btn0)
+        markup.add(btn1)
+        markup.add(btn2)
+        bot.send_message(message.from_user.id, 'Не очень понимаю запрос, я еще не выучил все слова на планете, выбери кнопку, пожалуйста🙏',reply_markup=markup)
+        bot.send_sticker(message.from_user.id, "CAACAgIAAxkBAAElicxk8KmBV9fzVoLzmGA_F7OVOrHR9AACoxcAAhfYoEhkgAYF4jHAQDAE")
+
+@bot.message_handler(content_types=['audio', 'document', 'sticker', 'video_note', 'voice', 'location', 'contact'])
+def get_other_messages(message):
+    get_msg_data(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn0 = types.KeyboardButton("Определить породу")
+    btn1 = types.KeyboardButton("Метрики")
+    btn2 = types.KeyboardButton("О проекте")
+    markup.add(btn0)
+    markup.add(btn1)
+    markup.add(btn2)
+    bot.send_message(message.from_user.id, 'Я не умею работать с такими форматами данных',reply_markup=markup)
+    bot.send_sticker(message.from_user.id, "CAACAgIAAxkBAAElicxk8KmBV9fzVoLzmGA_F7OVOrHR9AACoxcAAhfYoEhkgAYF4jHAQDAE")
+
+bot.polling(timeout = 90000)
---
+++ helpers.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+# import bbox_visualizer as bbv
+import os
+import glob
+from moviepy.editor import VideoFileClip
+from random import choice
+# import cv2
+from PIL import ImageDraw, Image
+from datetime import datetime
+import pytz
+
+def calculate_iou(bbox1,bbox2):
+    #todo add comment
+    bbox1,bbox2 = bbox1.xyxy.numpy()[0],bbox2.xyxy.numpy()[0]
+    w=0
+    h=0
+    if (bbox1[0] <= bbox2[2]) and (bbox2[0] <= bbox1[2]):
+        w = np.min((bbox1[2],bbox2[2])) - np.max((bbox1[0],bbox2[0]))
+        if (bbox1[1] <= bbox2[3]) and (bbox2[1] <= bbox1[3]):
+            h = np.min((bbox1[3],bbox2[3])) - np.max((bbox1[1],bbox2[1]))
+    overlap = w*h
+    area1 = (bbox1[2]-bbox1[0])*(bbox1[3]-bbox1[1])
+    area2 = (bbox2[2]-bbox2[0])*(bbox2[3]-bbox2[1])
+    iou = overlap/(area1+area2-overlap)
+    return iou
+
+
+def delete_overlaps(boxes,threshold):
+    #todo add comment
+    stop = False
+    bboxes = dict(enumerate(boxes))
+    if len(bboxes) <2:
+        stop = True
+    i = 0
+    j = 1
+    while not stop:
+        bbox1 = bboxes[i]
+        bbox2 = bboxes[j]
+        iou = calculate_iou(bbox1,bbox2)
+        if iou >= threshold:
+            # print(iou)
+            # print(cl_map[bbox1.cls.numpy()[0]],bbox1.conf.numpy())
+            # print(cl_map[bbox2.cls.numpy()[0]],bbox2.conf.numpy())
+            useless_box_id = np.argmin((bbox1.conf.numpy()[0],bbox2.conf.numpy()[0]))
+            # bboxes.pop(useless_box_id)
+            if useless_box_id == 0:
+                bboxes.pop(i)
+                i+= 1
+            else:
+                bboxes.pop(j)
+                j+= 1
+        else:
+            j+= 1
+        if j > max(list(bboxes.keys())):
+            i+= 1
+            j = i+1
+        if i >= sorted(list(bboxes.keys()))[-1]:
+            stop = True
+        # print(list(bboxes.values()))
+    return list(bboxes.values())
+
+
+
+def illustrate_boxes(preds,cl_map,threshold, img):
+    #todo add comment
+    colors = [(190, 110, 70),(198, 171, 123),(205, 231, 176),(163, 191, 168),(139, 163, 164),(114, 134, 160),(11, 79, 108)]
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    img1 = Image.fromarray(img)
+    draw = ImageDraw.Draw(img1)
+    boxes = delete_overlaps(preds.boxes,threshold) 
+    labels = []
+    bboxes = []
+    #unpack bboxes and labels from yolo 
+    for i in range(len(boxes)):
+        bbox = boxes[i].xyxy[0].numpy()
+        bbox = list(map(int,bbox))
+        bboxes.append(bbox)
+        label = cl_map[int(boxes[i].cls[0].numpy())] + '    ' + str(np.round(boxes[i].conf[0].numpy(),2))
+        labels.append(label)
+   
+    
+    used_labels = dict()
+    for l in range(len(labels)):
+        #same color for same class
+        short_label = labels[l].split('    ')[0]
+        if short_label in used_labels:
+            clr = used_labels[short_label]
+        else:
+            clr = choice(colors)
+            used_labels[short_label] = clr
+            colors.remove(clr)
+        
+        bbox = bboxes[l]
+        w_bbox = bbox[2]-bbox[0]
+        bbox_middle = w_bbox//2 + bbox[0]
+        #this way smol bboxes don't look ugly af
+        if w_bbox <= 100:
+            thickness = 1
+            size = 8
+        elif w_bbox <= 200:
+            thickness = 2
+            size = 12
+        elif min(img.shape[:1]) <= 450:
+            thickness = 4
+            size = 30
+        else:
+            thickness = 5
+            size = 35
+        
+        draw.rounded_rectangle(bbox,outline=clr,width=thickness,corners=(True,True,True,True),radius=4)
+        w_text = int(draw.textlength(labels[l],font_size=size))
+        #for label to stay within the bbox
+        while w_bbox < w_text:
+            size = size-1
+            w_text = int(draw.textlength(labels[l],font_size=size))
+        
+        text_bbox = draw.textbbox((bbox_middle,bbox[1]),labels[l],anchor='mt',font_size=size)
+        text_bbox = list(text_bbox)
+        text_bbox[0:3:2] = bbox[0:3:2]
+        text_bbox[3] += 2*thickness
+        draw.rounded_rectangle(text_bbox,fill=clr,outline=clr,width=thickness,corners=(True,True,True,True),radius=4)
+        draw.text((bbox_middle,bbox[1]+thickness),labels[l],anchor='mt',font_size = size, fill = (0,0,0))
+        
+        # cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]),clr,thickness)
+        # (label_width, label_height), baseline = cv2.getTextSize(label, font, size, thickness)
+        # label_bg = [bbox[0], bbox[1], bbox[0] + label_width+5, bbox[1] + label_height + int(14 * size)+ 0.5]
+        # cv2.rectangle(img, (label_bg[0], label_bg[1]),(label_bg[2] + 5, label_bg[3]), clr, -1)
+        # cv2.putText(img,label,(bbox[0] + 5, bbox[1] + int(16 * size) + (4 * thickness)), font, size, (0,0,0), thickness)
+
+    # clr = choices(colors,k=len(labels))
+    # clr = choice(colors)
+    # img1 = bbv.draw_multiple_rectangles(img,bboxes,thickness=thickness,bbox_color=clr)
+    # img1 = bbv.add_multiple_labels(img1,labels,bboxes,top=False,text_bg_color=clr)
+    return img1
+
+def read_class_map(path):
+    cl_map = pd.read_csv(path,index_col=0).to_dict()['label']
+    return cl_map
+
+def clear_temp_dirs():
+    temp_saved = glob.glob('saved_temp/*')
+    for f in temp_saved:
+        os.remove(f)
+    temp_vids = glob.glob('video_preds/temp/*')
+    for f in temp_vids:
+        os.remove(f)
+    
+
+def convert_avi_to_mp4(input_avi, output_mp4):
+    video = VideoFileClip(input_avi)
+    video.write_videofile(output_mp4)
+    os.remove(input_avi)
+
+
+def get_msg_data(message):
+    """prints and saves message information
+    format: user_name|massage text/type of message|%d/%m/%Y %H:%M:%S
+    """
+    msg_types = {'photo':'PHOTO','video':'VIDEO','audio':'audio','sticker':'sticker','video_note':'kruzhok','voice':'voice'}
+    if os.path.isfile('msg_logs.csv'):
+        df = pd.read_csv('msg_logs.csv', index_col=0)
+    else:
+        df = pd.DataFrame(columns = ['user_id', 'user_name', 'message_info', 'date'])
+    now = datetime.now(pytz.timezone('Europe/Moscow'))
+    now = now.strftime("%d/%m/%Y %H:%M:%S")
+    user_name = message.from_user.username
+    user_id = message.from_user.id
+    if message.content_type == 'text':
+        msg_info = message.text
+    elif message.content_type in msg_types:
+        msg_info = msg_types[message.content_type]
+    else:
+        msg_info = 'something went wrong'
+
+    df.loc[len(df.index)] = [user_id, user_name, msg_info, now]
+    df.to_csv('msg_logs.csv')
+    print('got new message: {} | {} | {}'.format(user_name,msg_info,now))
